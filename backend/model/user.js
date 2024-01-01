effe7f16bd3bb2dd83617a2f6578c5f20e0e36e6
Ankit Vishwakarma <ankit.coolzzzz@gmail.com>
2024-01-01 19:34:38 +0530

    login/register backend added ankit vishwakarma

---
+++ backend/model/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true
+    },
+    password: String, // Corrected from "passward" to "password"
+}, { timestamps: true });
+
+const UserModel = mongoose.model('User', UserSchema);
+module.exports = UserModel;
